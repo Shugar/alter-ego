f473b5628582356084a871d21a1813b6e3fa75d9
shugar <shugar348@gmail.com>
2019-10-10 05:20:00 +0300

    added some drag functionality to forms

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,64 @@
 <template>
   <div id="app">
-    <form class="form">
-      <div class="comment-header">Нашел здесь основную проблему нашей платформы</div>
-      <div class="comment-list">
-        <div
-          class="comment-item"
-          v-for="(item, index) in comments"
-          :key="index"
-        >
+    <div class="iframe-container">
+      <form
+        class="form"
+        :id="'formNumber' + item.x + '' + item.y"
+        v-for="(item, index) in forms"
+        :ref="'formNumber' + item.x + '' + item.y"
+        :key="index"
+        @mousedown="(e) => mainEH(e, ('formNumber' + item.x + '' + item.y))"
+        :style="{ left: item.x + 'px', top: item.y + 'px'}"
+      >
+        <!-- <div class="comment-header">
           <div class="comment-avatar"></div>
           <div class="comment-info">
-            <div class="comment-name">{{ item.name }}</div>
-            {{ item.comment }}
+            <div class="comment-name">{{ item.userData.name }}</div>
+            {{ item.userData.comment }}
           </div>
-        </div>
+        </div> -->
+        <div
+          class="form-close"
+          @click="forms.splice(index, 1)"
+        >x</div>
+        <div
+          class="comment-list"
+          :key="index"
+        >
+          <div
+            class="comment-item"
+            v-for="(item, index) in comments"
+            :key="index"
+          >
+            <div class="comment-avatar"></div>
+            <div class="comment-info">
+              <div class="comment-name">{{ item.name }}</div>
+              {{ item.comment }}
+            </div>
+          </div>
 
+        </div>
+        <input
+          v-model="commentText"
+          type="text"
+          placehoder="Введи коммент"
+        />
+        <button @click="addCommentToList()"> Опубликовать</button>
+      </form>
+      <div
+        @click="event => this.createForm(event)"
+        id="iframe-click-area"
+        ref="clickArea"
+        class="iframe-click-area"
+      >
       </div>
-      <input
-        v-model="commentText"
-        type="text"
-        placehoder="Введи коммент"
-      />
-      <button @click="addCommentToList()"> Опубликовать</button>
-    </form>
-    <iframe
-      ref="ta"
-      id="iframe"
-      src="https://app.matter.online"
-      seamless
-    ></iframe>
+      <iframe
+        ref="ta"
+        id="iframe"
+        src="https://app.matter.online"
+        seamless
+      ></iframe>
+    </div>
   </div>
 </template>
 
@@ -38,6 +68,12 @@ export default {
 
   data () {
     return {
+      isMouseDown: false,
+      amountCardId: [],
+      elementPosition: [0, 0],
+      mousePosition: [0, 0],
+      proccessingFormId: '',
+      forms: [],
       currentUser: {
         userId: 1,
         name: 'Tigran',
@@ -49,41 +85,106 @@ export default {
           usedId: 2,
           name: 'Katerine',
           comment: 'Hey there! I have some problems'
-        },
-        {
-          userId: 3,
-          name: 'Katerine',
-          comment: 'Hey there!'
-        },
-        {
-          userId: 4,
-          name: 'Katerine',
-          comment: 'Hey'
         }
       ]
     }
   },
 
   methods: {
-    select: event => {
-      console.log(event)
+    mainEH (event, name) {
+      this.mousePosition = [event.clientX, event.clientY]
+      this.proccessingFormId = name
+      this.elementPosition = [
+        this.forms.find(x => x.name === name).x,
+        this.forms.find(x => x.name === name).y
+      ]
+      document.addEventListener('mousemove', this.calcEH)
+      document.addEventListener('mouseup', this.removeHandlers)
+      document.addEventListener('contextmenu', this.removeHandlers)
     },
 
-    addCommentToList () {
+    calcEH (event) {
+      var vector = [
+        -this.mousePosition[0] + event.clientX,
+        -this.mousePosition[1] + event.clientY
+      ]
+
+      this.mousePosition = [
+        this.mousePosition[0] + vector[0],
+        this.mousePosition[1] + vector[1]
+      ]
+      this.elementPosition = [
+        this.elementPosition[0] + vector[0],
+        this.elementPosition[1] + vector[1]
+      ]
+
+      this.updatePosition()
+    },
+
+    removeHandlers () {
+      document.removeEventListener('mousemove', this.calcEH)
+      document.removeEventListener('mouseup', this.removeHandlers)
+      document.removeEventListener('contextmenu', this.removeHandlers)
+    },
+
+    updatePosition () {
+      console.log()
+      Object.keys(this.$refs).map(e => {
+        if (e === this.proccessingFormId) {
+          this.$refs[this.proccessingFormId][0].style.left =
+            this.elementPosition[0] + 'px';
+          this.$refs[this.proccessingFormId][0].style.top =
+            this.elementPosition[1] + 'px';
+        }
+      })
+    },
+
+    createForm (event) {
+      const x = event.clientX
+      const y = event.clientY
+
+      this.amountCardId = [x, y]
+      this.forms.push({
+        name: 'formNumber' + x + '' + y,
+        x,
+        y,
+        userData: {
+          ...this.currentUser
+        }
+      })
+    },
+
+    // handleMoveForm (event, item) {
+    //   if (this.isMouseDown) {
+    //     if (event.pageX > item.x) {
+    //       item.x = item.x + (event.pageX - item.x)
+    //       item.y = item.y + (event.pageY - item.y)
+    //     }
+    //   }
+    // },
+
+    // handleMouseDown (item, index) {
+    //   this.isMouseDown = true
+    // },
+
+    // handleMouseUp (item, index) {
+    //   this.isMouseDown = false
+    // },
+
+    addCommentToList (x, y) {
       this.comments.push({
+        x: x,
+        y: y,
         usedId: 5,
         name: this.currentUser.name,
         comment: this.commentText
       })
       this.commentText = null
+      this.isMouseDown = false
     }
   },
 
-  mounted () {
-    document
-      .getElementById('iframe')
-      .contentDocument.addEventListener('click', event => this.select(event))
-  }
+  mounted () {}
 }
 </script>
 
@@ -105,7 +206,7 @@ iframe {
   width: 100%;
   height: 100%;
   position: fixed;
-  z-index: 5;
+  z-index: -1;
   top: 0;
   left: 0;
 }
@@ -116,16 +217,15 @@ iframe {
   width: 100%;
   max-width: 320px;
   position: absolute;
-  right: 250px;
-  z-index: 999;
+  z-index: 2525;
   text-align: left;
-  border-radius: 10px;
   border: 1px solid #eee;
   box-shadow: 0 -4 -4 rgba(0, 0, 0, 0.9);
 }
 
 .comment-header {
   margin-bottom: 24px;
+  display: flex;
 }
 
 .content-list {
@@ -135,6 +235,18 @@ iframe {
 .comment-item {
   margin-bottom: 24px;
   display: flex;
+}
+
+.form-close {
+  content: "x";
+  font-weight: bold;
+  font-family: "Nunito", sans-serif;
+  font-size: 24px;
+  height: 24px;
+  width: 24px;
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 
 .comment-info {
@@ -155,6 +267,24 @@ iframe {
   height: 32px;
   background: red;
   border-radius: 100px;
+}
+
+.iframe-container {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: 200;
+  left: 0;
+  top: 0;
+}
+
+.iframe-click-area {
+  width: 100%;
+  height: 100%;
+  opacity: 1;
+  background: transparent;
+  top: 0;
+  left: 0;
 }
 
 input {
